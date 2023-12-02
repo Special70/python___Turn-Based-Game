9c4129362e1c0c79cf4590d05974a9c5634a77f7
Special70 <josef.urquico@gmail.com>
2023-12-02 16:55:47 +0800

    Removed Keypress Checker

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def keyListener(): # This function is only good at making inputs work. NOT at se
     while syslib.programStatus:
         if msvcrt.kbhit() and syslib.disableKeyListener == False:
             syslib.currentKey = str(msvcrt.getch()).split("'")[1].upper()
-            print(syslib.currentKey)
+            #print(syslib.currentKey) Only for debugging
             time.sleep(0.01) # This is important for the keybinds to work. Commenting this out bricks the controls. Why? The var resetter would overperform it's job of resetting the current keypress.
             if syslib.editorMode:
                 time.sleep(0.025) # The 0.025s time allows extra time for the program to record keypresses
